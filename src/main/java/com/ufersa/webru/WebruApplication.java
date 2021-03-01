8b0f1f0cae79f56d19ce6945a1d04ba7de2f1f97
vitorcardoso98 <vitorcardoso20131998@hotmail.com>
2021-02-28 21:57:29 -0300

    Inicial

---
+++ src/main/java/com/ufersa/webru/WebruApplication.java
@@ -0,0 +1,13 @@
+package com.ufersa.webru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebruApplication.class, args);
+	}
+
+}
