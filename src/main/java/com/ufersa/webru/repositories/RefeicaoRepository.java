b6513cf21fd4bd9e24f52d2c06ca9bbdea873033
vitorcardoso98 <vitorcardoso20131998@hotmail.com>
2021-05-31 15:00:47 -0300

    Corrigindo bug da escolha do mês do relatório histórico de consumo

--- src/main/java/com/ufersa/webru/repositories/RefeicaoRepository.java
+++ src/main/java/com/ufersa/webru/repositories/RefeicaoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ufersa.webru.model.Refeicao;
@@ -11,7 +12,7 @@ import com.ufersa.webru.model.Refeicao;
 @Repository
 public interface RefeicaoRepository extends CrudRepository<Refeicao, Long> {
 
-	@Query(value = "SELECT * from refeicao where DATE_PART('MONTH', data_refeicao) = ?1", nativeQuery = true)
-	List<Refeicao> getRelatorio(String mes);
+	@Query(value = "SELECT * from refeicao where DATE_PART('MONTH', data_refeicao) = :mes", nativeQuery = true)
+	List<Refeicao> getRelatorio(@Param("mes") String mes);
 
 }
